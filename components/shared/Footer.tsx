df2c24eeb076e70116ba3c71731185aa19abf12a
Ishan Veer <ishanpveer@gmail.com>
2025-05-22 11:42:41 +0530

    Created  footer

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,13 +1,62 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import Image from "next/image";
+
 import { navLinks, socialLinks } from "@/constants";
-import { usePathname } from "next/navigation";
 
 const Footer = () => {
   return (
-    <div className="px-10 md:px-28 py-8 bg-primary-500 text-white">Hello</div>
+    <div className="px-10 sticky bottom-0 md:px-16 py-20 flex justify-between items-start bg-primary-500 text-white">
+      {/* info container */}
+      <div className="flex w-1/4 flex-col gap-16">
+        <h1 className="title-font uppercase h1-light font-light">
+          lets work together
+        </h1>
+        <div className="title-font h3-regular flex flex-col gap-8">
+          <p>+91 65478 29364</p>
+          <p>sunindiarocks@gmail.com</p>
+          <p className="leading-8">
+            Sun Rock India123 Industrial Estate, NH-4Makrana Road, Kishangarh –
+            305801 Rajasthan, India
+          </p>
+        </div>
+      </div>
+      {/* link container */}
+      <div className="flex mt-6 gap-16">
+        {/* nav links */}
+        <div className="flex flex-col gap-6">
+          <h2 className="title-font text-2xl font-normal uppercase">
+            Quick Links
+          </h2>
+          <ul className="flex flex-col gap-4 h3-regular">
+            {navLinks.map((link) => (
+              <li key={link.route}>
+                <Link href={link.route}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        {/* social links */}
+        <div className="flex flex-col gap-6">
+          <h2 className="title-font text-2xl font-normal uppercase">
+            Social Links
+          </h2>
+          <ul className="flex capitalize flex-col gap-4 h3-regular">
+            {socialLinks.map((link) => (
+              <li key={link.route}>
+                <Link href={link.route}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      {/* Logo */}
+      <div className="">
+        <h1 className="title-font absolute top-32  -right-12 text-[400px] font-semibold">
+          S
+        </h1>
+      </div>
+    </div>
   );
 };
 
