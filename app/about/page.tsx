68986b1e7071581c6753f4389d2e86f38f90315d
Ishan Veer <ishanpveer@gmail.com>
2025-05-24 17:50:10 +0530

    removed unused div

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { excellence } from "@/constants";
-import { div } from "framer-motion/client";
 
 const Page = () => {
   return (
